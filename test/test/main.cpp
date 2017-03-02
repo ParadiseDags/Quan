2fa9142c60bdbcf1ba38c22b5701de0fc1d6ce33
永泉 刘 <paradisedags@hotmail.com>
2017-03-02 12:28:59 +0800

    修改暂停类型

--- test/test/main.cpp
+++ test/test/main.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
+#include<conio.h>
 using namespace std;
 int main()
 {
-	cout << "Hello GitHub..." << endl;
-	system("pause");
+	cout << "Hello GitHub...." << endl;
+	_getch();
 	return 0;
 }
